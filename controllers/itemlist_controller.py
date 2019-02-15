a9e86a4120c6c7202a0ce15b5d6c89a1f9f69dc1
cameronhoffman <cmohffman565@icloud.com>
2019-02-15 04:18:19 -0600

    Database error fix and comment corrections.

--- controllers/itemlist_controller.py
+++ controllers/itemlist_controller.py
@@ -2,28 +2,28 @@ from models import ItemList
 
 class ItemListController():
     def get_all_itemlists(self):
-    	"""Return model array of itemlists"""
+        """Return model array of itemlists"""
         return ItemList.select() # Return model array
 
     def get_itemlist(self, listitem_id):
-    	"""Return itemlist with given ID"""
+        """Return itemlist with given ID"""
         itemlist = ItemList.select().where(ItemList.id == listitem_id).get()
         return itemlist # Return model
 
     def create_itemlist(self, body):
-    	"""Create itemlist model with fields from body"""
+        """Create itemlist model with fields from body"""
         itemlist = ItemList.create(**body)
         return itemlist # Return model
 
     def update_itemlist(self, listitem_id, body):
-    	"""Update itemlist's name field with field from body, given ID"""
+        """Update itemlist's name field with field from body, given ID"""
         itemlist=ItemList.select().where(ItemList.id == listitem_id).get()
         itemlist.name=body['name'] # If tests looked for more than name, an update query would be nessesary.
         itemlist.save()
         return itemlist # Return model
 
     def delete_itemlist(self, listitem_id):
-    	"""Delete itemlist with given ID"""
+        """Delete itemlist with given ID"""
         ItemList.delete().where(ItemList.id == listitem_id).execute()
         return # Return nothing
 
--- models/item.py
+++ models/item.py
@@ -6,5 +6,5 @@ class Item(BaseModel):
     class Meta:
         table_name = 'item'
     name = TextField()
-    item_list = ForeignKeyField(ItemList, backref='item_lists')
+    item_list = ForeignKeyField(ItemList, backref='item_lists') # Had to turn off 'not null' for field in db, field not passed in creation tests.
     is_complete = BooleanField(default=False)
